60f7a9859ec15c1b7076102c80c3fae27a4cfd77
stivesd <stivenjdk@gmail.com>
2024-01-23 15:14:25 -0500

    GoogleMaps

---
+++ server/app.js
@@ -0,0 +1,77 @@
+import express from "express";
+import {
+  getTodo,
+  shareTodo,
+  deleteTodo,
+  getTodosByID,
+  createTodo,
+  toggleCompleted,
+  getUserByEmail,
+  getUserByID,
+  getSharedTodoByID,
+} from "./database.js";
+import cors from "cors";
+
+const corsOptions = {
+  origin: "http://127.0.0.1:5173", // specify the allowed origin
+  methods: ["POST", "GET"], // specify the allowed methods
+  credentials: true, // allow sending credentials (cookies, authentication)
+};
+
+const app = express();
+app.use(express.json());
+app.use(cors(corsOptions));
+
+app.get("/todos/:id", async (req, res) => {
+  const todos = await getTodosByID(req.params.id);
+  res.status(200).send(todos);
+});
+
+app.get("/todos/shared_todos/:id", async (req, res) => {
+  const todo = await getSharedTodoByID(req.params.id);
+  const author = await getUserByID(todo.user_id);
+  const shared_with = await getUserByID(todo.shared_with_id);
+  res.status(200).send({ author, shared_with });
+});
+
+app.get("/users/:id", async (req, res) => {
+  const user = await getUserByID(req.params.id);
+  res.status(200).send(user);
+});
+
+app.put("/todos/:id", async (req, res) => {
+  const { value } = req.body;
+  const todo = await toggleCompleted(req.params.id, value);
+  res.status(200).send(todo);
+});
+
+app.delete("/todos/:id", async (req, res) => {
+  await deleteTodo(req.params.id);
+  res.send({ message: "Todo deleted successfully" });
+});
+
+app.post("/todos/shared_todos", async (req, res) => {
+  const { todo_id, user_id, email } = req.body;
+  // const { todo_id, user_id, shared_with_id } = req.body;
+  const userToShare = await getUserByEmail(email);
+  const sharedTodo = await shareTodo(todo_id, user_id, userToShare.id);
+  res.status(201).send(sharedTodo);
+});
+
+// app.get("/todos/:id", async (req, res) => {
+//   const id = req.params.id;
+//   const todo = await getTodo(id);
+//   res.status(200).send(todo);
+// });
+
+app.post("/todos", async (req, res) => {
+  const { user_id, title } = req.body;
+  const todo = await createTodo(user_id, title);
+  res.status(201).send(todo);
+});
+
+app.listen(8080, () => {
+  console.log("Server running on port 8080");
+});
+
+////////////////////////////////////////////////////////////////////
---
+++ server/schema.sql
@@ -0,0 +1,57 @@
+CREATE DATABASE todo_tutoriall;
+
+USE todo_tutoriall;
+
+CREATE TABLE users (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255),
+  email VARCHAR(255) UNIQUE NOT NULL,
+  password VARCHAR(255)
+);
+
+CREATE TABLE todos (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(255),
+  completed BOOLEAN DEFAULT false,
+  user_id INT NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE 
+);
+
+CREATE TABLE shared_todos (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  todo_id INT,
+  user_id INT,
+  shared_with_id INT,
+  FOREIGN KEY (todo_id) REFERENCES todos(id) ON DELETE CASCADE,
+  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+  FOREIGN KEY (shared_with_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+
+
+INSERT INTO users (name, email, password) VALUES ('Beto', 'user1@example.com', 'password1');
+INSERT INTO users (name, email, password) VALUES ('Alberto', 'user2@example.com', 'password2');
+
+-- Insert todos into the todos table, associated with the first user
+INSERT INTO todos (title, user_id) 
+VALUES 
+("🏃‍♀️ Go for a morning run 🌄", 1),
+("💻 Work on project presentation 💼", 1),
+("🛒 Go grocery shopping 🛍️", 1),
+("📚 Read 30 pages of book 📖", 1),
+("🚴‍♂️ Ride bike to the park 🌳", 1),
+("🍲 Cook dinner for family 🍴", 1),
+("💆‍♂️ Practice yoga 🧘‍♂️", 1),
+("🎧 Listen to a podcast 🎤", 1),
+("🧹 Clean the house 🧼", 1),
+("🛌 Get 8 hours of sleep 💤", 1);
+
+-- share todo 1 of user 1 with user 2
+INSERT INTO shared_todos (todo_id, user_id, shared_with_id) 
+VALUES (1, 1, 2);
+
+-- Get todos including shared todos by id
+SELECT todos.*, shared_todos.shared_with_id
+FROM todos
+LEFT JOIN shared_todos ON todos.id = shared_todos.todo_id
+WHERE todos.user_id = [user_id] OR shared_todos.shared_with_id = [user_id];
